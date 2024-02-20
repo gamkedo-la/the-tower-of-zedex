360810c717292d7441d2eefa865f3b3dd72e1f72
Christer Kaitila <breakdance@mcfunkypants.com>
2024-02-20 14:31:23 -0800

    wall huggers now navigate along wall edges!

--- js/wallHugger.js
+++ js/wallHugger.js
@@ -2,46 +2,34 @@
 // traces around the walls it finds
 
 function wallHuggerClass() {
-	this.enemyType;
+	
+    this.enemyType = "Wall Hugger";
 	
 	this.x = 0;
 	this.y = 0;
-	this.speed = 1.5;
+	this.speed = 1;
 	
 	// Stats
-	this.readyToRemove =    false;
-	this.maxHealth =      2;
-	this.health =         0;
+	this.readyToRemove =  false;
+	this.maxHealth =      9999999; // invulnerable
+	this.health =         9999999;
 	this.attackDamage =   2;
 
 	// Direction
-	this.direction =  'south';
-	this.walkNorth =  false;
+	this.walkNorth =  true;
 	this.walkEast =   false;
-	this.walkSouth =  true;
+	this.walkSouth =  false;
 	this.walkWest =   false;
-
-	// Move frequency
-	this.ticksFromLastMovement =	0;
-	this.ticksUntilNextMovement =	2;
-	this.ticksFromLastDirectionChange = 0;
-	this.ticksUntilDirectionChange = 60;
-
-	this.distanceToChase = 100;
-	this.chasePlayer = false;
 
 	// Animation stuff
 	this.sprite;
-	this.frameIndex =      0;
-	this.tickCount =       0;
-	this.ticksPerFrame =  15;
+	this.frameIndex =     0;
+	this.tickCount =      0;
+	this.ticksPerFrame =  5;
 
-
-	this.init = function( enemyPic ) {
-        this.health = this.maxHealth;
-		this.readyToRemove = false;
-		this.sprite = enemyPic;
-		this.reset();
+	this.init = function( enemyPicUnused ) {
+		this.sprite = wallHuggerSprite1;
+        this.reset();
 	}
 
 	this.reset = function(resetX, resetY) {
@@ -50,9 +38,9 @@ function wallHuggerClass() {
 			if( roomGrid[i] == TILE_WALL_HUGGER) {
 				var tileRow = Math.floor(i/ROOM_COLS);
 				var tileCol = i%ROOM_COLS;
-				this.homeX = tileCol * TILE_W + 0.5*TILE_W;
-				this.homeY = tileRow * TILE_H + 0.5*TILE_H;
-				roomGrid[i] = TILE_GROUND;
+				this.homeX = tileCol * TILE_W;
+				this.homeY = tileRow * TILE_H;
+				roomGrid[i] = TILE_CRYPT_WALL1; // TILE_GROUND; (since it is inside the wall)
                 console.log("spawning a wall hugger at "+this.homeX+","+this.homeY);
 				break; // found it, so no need to keep searching 
 			} // end of if
@@ -64,107 +52,38 @@ function wallHuggerClass() {
 		this.readyToRemove =  false;
 	}
 
-
-
-	this.changeDirection = function(direction) {
-		if( direction != undefined ) {
-			this.direction = direction;
-			return;
-		}
-		var randomDirection = Math.ceil(Math.random()*4);
-		
-		if (randomDirection == 1)		{this.direction = 'north'}
-		else if (randomDirection == 2)	{this.direction = 'east'}
-		else if (randomDirection == 3)	{this.direction = 'south'}
-		else if (randomDirection == 4)	{this.direction = 'west'}
-		else { 
-			console.log('random direction error', randomDirection, "default direction to south");
-			this.direction = 'south'
-		}
-	}
-
-		
-
 	this.move = function() {	
-
-		// working on a chase movement pattern
-		if( Math.abs(this.x - p1.x) <= this.distanceToChase ) {
-			this.chasePlayer = true;
-		} else { this.chasePlayer = false; }
-
-		if( this.chasePlayer == true ) {
-			if( p1.x > this.x ) {
-				this.changeDirection('east');
-			} else if( p1.y > this.y ) {
-				this.changeDirection('north');
-			} else if( p1.x < this.x ) {
-				this.changeDirection('west');
-			} else {
-				this.changeDirection('south');
-			}
-		}
 
 		var nextX = this.x;
 		var nextY = this.y;
-		var nextTileX = this.x;
-		var nextTileY = this.y;
 
-			
-		// next X/Y depending on direction
-		if(this.direction == 'north') {
-			nextY -= this.speed;
-			nextTileY = this.y-1;
-		}
-		if(this.direction == 'east') {
-			nextX += this.speed;
-			nextTileX = this.x + TILE_W;
-		}
-		if(this.direction == 'south') {
-			nextY += this.speed;
-			nextTileY = this.y + TILE_H;
-		}
-		if(this.direction == 'west') {
-			nextX -= this.speed;
-			nextTileX = this.x - 1;
-		}
+		var standingOnTileType = roomGrid[getTileIndexAtPixelCoord(nextX,nextY)];
+        //if (standingOnTileType) console.log("wallhugger is standing on a "+standingOnTileType+" at "+this.x+","+this.y);
 
-		var walkIntoTileIndex = getTileIndexAtPixelCoord(nextTileX,nextTileY);
-		var walkIntoTileType = TILE_WALL;
-		
-		if( walkIntoTileIndex != undefined) {
-			walkIntoTileType = roomGrid[walkIntoTileIndex];
-		}
+        if (this.walkNorth) nextY -= this.speed;
+        if (this.walkSouth) nextY += this.speed;
+        if (this.walkEast) nextX += this.speed;
+        if (this.walkWest) nextX -= this.speed;
 
-		switch( walkIntoTileType ) {
-			case TILE_GROUND:
-				this.x = nextX;
-				this.y = nextY;
-				break;
-			case TILE_GOAL:
-				this.x = nextX;
-				this.y = nextY;
-				break;
-			case TILE_KEY:
-				this.x = nextX;
-				this.y = nextY;
-				break;
-			case TILE_SPIKE:
-				this.x = nextX;
-				this.y = nextY;
-				break;
-			case TILE_WALL:
-			default:
-				// any other tile type number was found... do nothing, for now
-				break;
-		}
-		this.ticksFromLastMovement = 0;
-		
+		var walkIntoTileType = roomGrid[getTileIndexAtPixelCoord(nextX,nextY)];
+        //if (walkIntoTileType) console.log("wallhug about to hit tile "+walkIntoTileType);
+
+        if (!walkIntoTileType) {
+            //console.log("wallhugger sees floor at "+nextX+","+nextY+" - changing directions!");
+            // floor ahead! try another way
+            if (this.walkNorth) { this.walkNorth=false; this.walkEast=true; }
+            else if (this.walkEast) { this.walkEast=false; this.walkSouth=true; }
+            else if (this.walkSouth) { this.walkSouth=false; this.walkWest=true; }
+            else if (this.walkWest) { this.walkWest=false; this.walkNorth=true; }
+        } else {
+            // keep going - there is more wall up ahead
+            this.x = nextX;
+            this.y = nextY;
+        }
 		
 	}
 
 	this.draw = function() {
-		// drawRect(this.x, this.y, 32,32, 3, 'green');
-
 		this.tickCount++;
 		if(this.tickCount == this.ticksPerFrame){
 			this.tickCount = 0;
@@ -173,7 +92,14 @@ function wallHuggerClass() {
 			} else { this.sprite = wallHuggerSprite1 }
 		}
 		
-		canvasContext.drawImage( this.sprite, this.x, this.y );
+        if (this.walkNorth) this.rotation = 0;
+        if (this.walkEast) this.rotation = 90 * (Math.PI/180);
+        if (this.walkSouth) this.rotation = 180 * (Math.PI/180);
+        if (this.walkWest) this.rotation = 270 * (Math.PI/180);
+        drawBitmapCenteredAtLocationWithRotation(this.sprite,this.x,this.y,this.rotation);
+        
+        // unrotated, works great:
+        // canvasContext.drawImage( this.sprite, this.x-16, this.y-16 ); // sprite is centered on its xy
 	}
 
 	
